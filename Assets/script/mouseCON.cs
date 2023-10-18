8d295811f71e2131c01e06dd4fd39a7842e3462d
naritaituki <maasu0611@gmail.com>
2023-10-18 10:38:41 +0900

    公文式課題６、マウス位置の取得

---
+++ Assets/script/mouseCON.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class mouseCON : MonoBehaviour
+{
+    // 位置座標
+    private Vector3 position;
+    // スクリーン座標をワールド座標に変換した位置座標
+    private Vector3 screenToWorldPointPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3でマウス位置座標を取得する
+        position = Input.mousePosition;
+        // Z軸修正
+        position.z = 10f;
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        // ワールド座標に変換されたマウス座標を代入
+        gameObject.transform.position = screenToWorldPointPosition;
+    }
+}
